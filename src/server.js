e4beeacef73ec8f9212f1d35def859eda97da7d0
JuliaPBV <vital.pereira.julia@gmail.com>
2025-04-22 13:32:17 -0300

    criando tarefa

--- src/server.js
+++ src/server.js
@@ -1,1 +1,30 @@
+import { create } from 'domain'
 import http from 'http'
+
+const tasks = []
+
+const server = http.createServer((req, res) => {
+    const { method, url } = req
+
+    if (method == 'GET' && url == '/tasks') {
+        return res.end(JSON.stringify(tasks))
+    }
+
+    if (method == 'POST' && url == '/tasks') {
+        tasks.push({
+            id: 1,
+            title: 'lavar roupa',
+            description: 'Lavar roupas brancas',
+            completed_at: null,
+            created_at: new Date(),
+            updated_at: new Date(),
+        })
+
+        return res.end('Criação de lista de tarefas')
+
+    }
+
+    return res.end('Hello Julia')
+})
+
+server.listen(3333)
